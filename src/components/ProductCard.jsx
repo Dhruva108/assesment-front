5b778894687a6e57e23057696d1e318bd8090a53
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-07-27 23:58:57 -0500

    Modify and continues developping of assesment

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-const ProductCard = () => {
+const ProductCard = ({ list }) => (
   <div>
-    ProductCard
-  </div>;
+    {
+      list.map((item) => (
+        <div key={item.id} className>
+          <Link to={`/detail${item.id}`}>
+            <h1>{item.title}</h1>
+            <img src={item.img} alt="" />
+          </Link>
+        </div>
+      ))
+    }
+  </div>
+);
+
+ProductCard.propTypes = {
+  list: PropTypes.arrayOf(
+    PropTypes.shape(),
+  ).isRequired,
 };
 
 export default ProductCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import ProductCard from '../components/ProductCard';
+import { getProducts } from '../services/products';
 
-const Home = () => (
-  <h1>Aquí va algo</h1>
+const Home = () => {
+  const [product, setProduct] = useState([]);
 
-);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await getProducts();
+      setProduct(result);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <ProductCard list={product} />
+  );
+};
 
 export default Home;
